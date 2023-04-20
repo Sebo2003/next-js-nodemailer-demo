a601f73740196871c10b3455791be0f7d7bc19e8
Sebastian Przybylski <generalg2003@gmail.com>
2023-04-20 14:14:02 -0500

    removed-comments

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,88 +1,26 @@
-/**
- * In Next.js, all files within the api folder is treated as an API endpoint instead of a page.
- * This allows us to seemesly write backend code without interupting the processes of frontend.
- * I reccomend vercel's documentation on this: https://nextjs.org/docs/api-routes/introduction
- * contact.js serves the purpose of:
- * 1) receiving the form data
- * 2) constructing an email with said data
- * 3) sending the data to the proper email endpoint
- * 
- * Here is another article I highly reccomend reading: https://refine.dev/blog/next-js-api-routes/
- */
-
-//import the nodemailer objects we configured earlier
-import { mailOptions, transporter } from "../../config/nodemailer";
-
-/**
- * (I reccomend you read the comments below this function first and come back to this paragraph, it'll
- * probably make more sense then)
- * 
- * This function accepts the arguement 'data', as parsed from /lib/api.js, and then accumaltes the
- * data therewithin to print a long string of data
- */
 const generateEmailContent = (data) => {
-  /**
-   * 'stringData' is our accumlator variables, we will append text to it recursivley until it is done
-   * parsing through all the keys of 'data'
-   */
+
   const stringData = Object.entries(data).reduce(
     (str, [key, val]) =>
-    //We call '\n' to add a new line inbetween appends for readability
       (str += `${key}: \n${val} \n \n`),"");
   return {
     text: stringData,
-    //optional html data (refer to the finished-files branch)
   };
 };
 
-/**
- * This 'handler' function is responsible for handling the requests made to this endpoint (contact.js, that is).
- * It has two arguements: 'req' and 'res' (these should be familiar if you've ever worked with Express.js or
- * Node). 'req' is an instance of the incoming HTTP request, and 'res' is an instance of the HTTP server 
- * response. These two instances come bundled with some convientnet middleware that allows us to parse &
- * manipulate our data better.
- */
 const handler = async (req, res) => {
-  /**
-   * Check if our HTTP request is a POST method. If it is - execute the code within the function.
-   * Else, return a 400 HTTP error code of 'bad request'. In other words: "if this ain't a POST
-   * method, something has gone wrong - don't execute the code any further and tell the server
-   * there has been some sort of bad request error"
-   */
   if (req.method === "POST") {
-    //'req.body' correlates to the whole contents of our request (which is the contents of 'values' from index.jsx)
     const data = req.body;
-    //Send an error response if some piece of the body code is empty (in the case someone submitted missing info somehow)
     if (!data || !data.name || !data.email || !data.subject || !data.message) {
       return res.status(400).send({ message: "Bad request" });
     }
-
-    /**
-     * Our 'transporter' object was already constructed in /config/nodemailer, remember it is a 
-     * nodemailer object. We call the 'sendMail' function from nodemailer to actually send our 
-     * data. We put within a try/catch block to try to send the mail first, and to catch &
-     * errors we encoutner, and then return a bad status with a json message reporting the error.
-     * We 'await' transporter.sendMail() because our handler function is asynchronous, and we need
-     * to wait to return our promise.
-     */
     try {
       await transporter.sendMail({
-        //spread out mailOptions, which if you'll remember, is a data structure containing who the mail is from, and to who its going to
         ...mailOptions,
-        /**
-         * Call the generateEmailContent, which accepts data as its arguement (which we passed in /lib/api.js).
-         * The function will return our the value of the nodemailer 'text' property (which needs to be to be
-         * filled non empty if we want the email sent to actually contain the form data). This function can
-         * also be configured to return HTML elements as well, as more thoroughly explained in the
-         * video tutorial in index.js.
-         */
         ...generateEmailContent(data),
-        //The 'subject' property is a nodemailer property that fills the subject of the email. We pass it with our 'subject' field from 'data'.
         subject: data.subject,
       });
-      //If the email was sent A-OK: send a HTTP 200 response of "everything went good"
       return res.status(200).json({ success: true });
-      //catch the error, log it, and display its JSON contents:
     } catch (err) {
       console.log(err);
       return res.status(400).json({ message: err.message });
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 /**
- * Sample code to guide the AP Pro Inc team at the COD CS club in Nodemailer,
  * based off of this video: https://www.youtube.com/watch?v=t2LvPXHLrek&t=960s
  */
 import Head from 'next/head'
 import { useState } from 'react'
 import { sendContactForm } from '/lib/api';
 
-//This const creates an initial set of values (an object), all of whom are empty, for the upcoming useState instance
 const initValues = {
   name:"", 
   subject:"",
@@ -14,59 +12,22 @@ const initValues = {
   message:""
 }
 
-//This sets the value of "values" equal to initVlaues, or in other words, equal to empty parameters for all values
 const initState = {values:initValues}
 
 export default function Home() {
-
-  /**
-   * we call the useState hook to be able to update the state of the input fields, where 'state' is the current state,
-   * 'setState' is the value we are updating to, and 'initState' (which we discussed before) is the initial value being passed
-   */
   const [state, setState] = useState(initState);
-   //we can assign 'values' to equal the current state of our field (as defined in line 24)
    
   const {values} = state;
 
-  /**
-   * (It might make more sense to read the second paragraph's comment beneath the </Head> tag first to understand this better)
-   * This function handles the change of the input fields. It calls upon 'setState' from our instance of
-   * useState to pass a function whose parameter is the old state, called 'prev'.
-   */
   const handleChange = ({target}) => 
     setState((prev) => ({
-      /**
-       * The ellipse operator is a Javascript syntax feature that allows use 'spread syntax',
-       * basically, its used as a sort of implication that we are smearing the some value to
-       * every member of some sort of data structure (indexes in an array for example)
-       * This article uses some good examples (make sure you're looking at the 'spread syntax' portion,
-       * although its good to know both): https://betterprogramming.pub/javascript-ellipses-the-spread-and-rest-syntax-c12df294548d
-       * The reason why we are doing this is to retain any properties within the initState object
-      */
       ...prev, 
-      //we are now overriding the values property
       values:{
-        //spread out the previous state's values, so if only on property is updated ('name' for example) - the rest don't get wiped
         ...prev.values,
-        /**
-         * we use 'target' as a dynamic variable of sorts (notice we passed it in this function).
-         * Notice each input tag in the code below has a 'name' property. Whatever that 'name' 
-         * proerty is equal to, we can set equal to 'target'. Then, we can assign that target a value.
-         */
         [target.name]: target.value
       }
   }));
 
-  /**
-   * This is what is called when we click the 'submit' button at the bottom of the form. 
-   * Notice the 'async' and 'await' keywords - they are foundational in REST API's.
-   * Essentially, these keywords are used to resolve promises in Javascript. 
-   * 'sendContactForm' is waiting for onSubmit to return a promise, which is activated
-   * when the user clicks the button at the buttom of the form (you can think of it as
-   * this function's event handler). Here's a quick guide:
-   * https://www.geeksforgeeks.org/async-await-function-in-javascript/#
-   * 
-   */
   const onSubmit = async () => {
     try {
       await sendContactForm(values);
@@ -82,21 +43,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {
-        /**
-         * The code below is a simple HTML form that does NOT rely upon any UI components. Take notice of
-         * each input tag. They each have a 'value' field that is equal to 'value.X', where 'X' the tag's
-         * name. Remember what the variable 'value' is: it is a reference to the state property it the
-         * useState instance. We want the value of each tag to be equal to the value of the current 
-         * state, which was called 'state'. Because 'values' is equal to the current state, we can
-         * reference the fields of initValues directly through the variable 'values', as opposed of
-         * doing 'state.values' every time. 
-         * 
-         * However, input tags are not able to change their state on their own, so each input tag
-         * also needs an 'onChange' field to able to update their state. Each input calls the
-         * function 'handleChange' every time it is changed.
-         */
-      }
       <main>
        <h1>Contact:</h1>
        <form>
